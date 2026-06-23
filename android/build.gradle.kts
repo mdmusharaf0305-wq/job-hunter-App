0d53cd4c1a06f38b63c41d84376d4ed54b93f718
Mohammed Musharraf <md.musharaf0305@gmail.com>
2026-06-23 21:19:14 +0530

    fix: use reflection for subproject namespace fallback configuration in Kotlin DSL build script

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,11 +21,17 @@ subprojects {
 
 subprojects {
     afterEvaluate {
-        if (extensions.findByName("android") != null) {
-            configure<com.android.build.gradle.BaseExtension>("android") {
-                if (namespace == null) {
-                    namespace = "com.musharraf.jobhunter.${project.name.replace("-", "_")}"
+        val androidExtension = extensions.findByName("android")
+        if (androidExtension != null) {
+            try {
+                val getNamespace = androidExtension.javaClass.getMethod("getNamespace")
+                val namespaceValue = getNamespace.invoke(androidExtension)
+                if (namespaceValue == null) {
+                    val setNamespace = androidExtension.javaClass.getMethod("setNamespace", String::class.java)
+                    setNamespace.invoke(androidExtension, "com.musharraf.jobhunter.${project.name.replace("-", "_")}")
                 }
+            } catch (e: Exception) {
+                // Ignore if namespace property or methods are not present
             }
         }
     }
